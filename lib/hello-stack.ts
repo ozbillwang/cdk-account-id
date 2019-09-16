b7843c4a984132c9aa9a88d847dcce95e76abe4a
Bill Wang <bill.wang>
2019-09-16 12:20:40 +1000

    output account id

--- lib/hello-stack.ts
+++ lib/hello-stack.ts
@@ -4,6 +4,10 @@ export class HelloStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Publish the custom resource output
+    new cdk.CfnOutput(this, "MyStackAccount", {
+      description: "Account of this stack",
+      value: this.account
+    });
   }
 }
